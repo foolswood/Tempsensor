cd2f84696a0b2d98cc0a7e35b66817460a97e4a3
David Honour <davhonou@cisco.com>
2015-10-22 07:35:18 +0100

    Pastebin from Haskey

---
+++ tempsense.c
@@ -0,0 +1,45 @@
+	
+
+     
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <wiringPi.h>
+     
+     
+    int main (void)
+    {
+            wiringPiSetup(); //Setup WiringPi
+            pinMode (15, INPUT);
+            pinMode (16, OUTPUT);
+     
+            int read[8];
+            int x,y; //y is genuinely ony there for running reasons
+            system("echo -n \"$(date +%s)\""); //Bad practice I know but can't be arsed to engage the full C date stuff
+     
+            printf(",");
+     
+    //      int y;
+    //      for (y = 0; y < 3; y++)
+    //{
+    //      getchar();
+            for(x = 0; x <= 8; x++) //for 8 bits
+            {
+                    digitalWrite(16,HIGH);
+                    delay(10);
+                    y = x; //Genuinely not used. Remove this and the code stops working.
+                    read[x] = !digitalRead(15);
+    //              printf("%d, %d \n",read[x],x);
+                    digitalWrite(16,LOW);
+                    delay(10);
+            }
+            x = 10;
+            for(x = 8; x >= 0; x--)
+            {
+                    printf("%d",read[x]); //print out the 1's and 0's in the opposite order they are read in (are read backwards)
+            }
+            printf("\n");
+     
+            return 0;
+    }
+
+
